f6d4512b493258934c4be8d8fe14548ac3e84f88
scott-wyatt <scottwyatt86@gmail.com>
2018-07-23 23:39:25 -0400

    [fix] normalize prefix to standard

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,17 +1,29 @@
 export const Routes = {
   '/{model}': {
+    config: {
+      prefix: 'tapestries.prefix'
+    },
     'POST': 'TapestryController.create'
   },
   '/{model}/{id?}': {
+    config: {
+      prefix: 'tapestries.prefix'
+    },
     'GET': 'TapestryController.find',
     'PUT': 'TapestryController.update',
     'PATCH': 'TapestryController.update',
     'DELETE': 'TapestryController.destroy'
   },
   '/{parentModel}/{parentId}/{childAttribute}': {
+    config: {
+      prefix: 'tapestries.prefix'
+    },
     'POST': 'TapestryController.createAssociation'
   },
   '/{parentModel}/{parentId}/{childAttribute}/{childId?}': {
+    config: {
+      prefix: 'tapestries.prefix'
+    },
     'GET': 'TapestryController.findAssociation',
     'PUT': 'TapestryController.updateAssociation',
     'DELETE': 'TapestryController.destroyAssociation'
